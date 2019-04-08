4d3e127d8f55433e8dc107dfe338da95ce7996e6
reesefairbanks <reese005@gmail.com>
2019-04-08 18:03:36 -0400

    Updates

---
+++ precache-manifest.6d591d41ea08b149cbbcbcfc4af72a86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d466a58c7b726ee0dfe",
+    "url": "/clicky-game/static/css/main.f827d801.chunk.css"
+  },
+  {
+    "revision": "0d466a58c7b726ee0dfe",
+    "url": "/clicky-game/static/js/main.0b8a571b.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0dfa507646545a33da99c56e418e5ab9",
+    "url": "/clicky-game/index.html"
+  }
+];
